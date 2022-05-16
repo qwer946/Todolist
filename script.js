944ed62ef09c900346c6f7b58d5690ed71894db0
qwer946 <khalid.ali.987@gmail.com>
2022-05-16 11:15:30 -0500

    append child and spaced the text with ""

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let todolists = document.querySelector(".todolists");
 
 btns.addEventListener("click", function () {
   let lists = document.createElement("para");
+  lists.classList.add("parastyle");
   lists.innerText = input.value;
-  todolists.append(lists);
+  todolists.appendChild(lists);
+  input.value = " ";
 });
