9f18a4fcbef461aef65115802156815856047c25
qwer946 <khalid.ali.987@gmail.com>
2022-05-16 10:31:33 -0500

    adding input value

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let input = document.querySelector("#input");
 let btns = document.querySelector("#btns");
-let todolists = document.querySelector("#todolists");
+let todolists = document.querySelector(".todolists");
+
+btns.addEventListener("click", function () {
+  let lists = document.createElement("para");
+  lists.innerText = input.value;
+});
